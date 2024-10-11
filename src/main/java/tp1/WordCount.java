eecc8a2bc7ee42bc5be40764c5b7ca008bb3fd12
azlouk96 <ahmedazlouk9@gmail.com>
2024-10-11 17:09:15 +0100

    commited

---
+++ src/main/java/tp1/WordCount.java
@@ -0,0 +1,27 @@
+package tp1;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job.*;
+import org.apache.log4j.BasicConfigurator;
+
+public class yWordCount {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        BasicConfigurator.configure();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
